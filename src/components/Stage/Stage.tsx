28e32a17f62b0f757fe4506edd3a76498abdee42
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2024-08-22 23:52:48 +0200

    implemented Stage which displays stage

---
+++ src/components/Stage/Stage.tsx
@@ -0,0 +1,35 @@
+import { Box, styled } from '@mui/material';
+import uuid from 'react-uuid';
+import React from 'react';
+import { Cell } from '../Cell';
+
+type Props = {
+  stage: any,
+};
+
+type StyledBoxCustomProps = {
+  width: number,
+  height: number,
+};
+
+const StyledBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'width' && prop !== 'height',
+})<StyledBoxCustomProps>(({ width, height }) => ({
+  display: 'grid',
+  gridTemplateRows: `repeat(${height}, calc(25vw / ${width}))`,
+  gridTemplateColumns: `repeat(${width}, 1fr)`,
+  gridGap: '1px',
+  border: '2px solid #333',
+  width: '100%',
+  maxWidth: '25vw',
+  background: '#111111',
+}));
+
+export const Stage: React.FC<Props> = ({ stage }) => (
+  <StyledBox
+    width={stage[0].length}
+    height={stage.length}
+  >
+    {stage.map(row => row.map((cell) => (<Cell key={uuid()} type={cell[0]} />)))}
+  </StyledBox>
+);
